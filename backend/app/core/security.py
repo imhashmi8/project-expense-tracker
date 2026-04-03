ee3d3117ff79ff475e57afd1e4141b03ddf37568
imhashmi8 <qamar.hashmi@outlook.com>
2026-04-03 17:42:03 +0530

    fixing password hashing issue

--- backend/app/core/security.py
+++ backend/app/core/security.py
@@ -7,7 +7,7 @@ from passlib.context import CryptContext
 from app.core.config import get_settings
 
 settings = get_settings()
-password_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+password_context = CryptContext(schemes=["pbkdf2_sha256"], deprecated="auto")
 
 
 def hash_password(password: str) -> str:
